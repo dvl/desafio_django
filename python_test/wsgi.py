7589a0f05337704f53c59984878d33d56b632afe
André Luiz <contato@xdvl.info>
2015-11-22 15:43:43 -0200

    usa o template do 1.8+

--- python_test/wsgi.py
+++ python_test/wsgi.py
@@ -1,6 +1,7 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python_test.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python_test.settings")
 
 application = get_wsgi_application()
